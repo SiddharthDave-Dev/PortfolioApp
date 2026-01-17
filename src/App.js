f4b0e38007c17446c24ec23712c023e541667a2a
SiddharthDave-Dev <davesiddharth76@gmail.com>
2026-01-18 00:12:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Skills from './components/Skills/Skills';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
-import ParticlesBackground from './components/ParticlesBackground';
 import './App.css';
 
 function App() {
